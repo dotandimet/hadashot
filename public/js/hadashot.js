71c27e62ceca6fa351e1062f2fccb4b652690481
Dotan Dimet <dotan@corky.net>
2013-09-12 12:34:42 +0300

    stuff

--- public/js/hadashot.js
+++ public/js/hadashot.js
@@ -19,6 +19,7 @@ var hadashot = (function(jq,me){
   me.tmpl = function tmpl(str, data) {
 // Simple JavaScript Templating
 // John Resig - http://ejohn.org/ - MIT Licensed
+// Dotan Dimet - modifications: dropped "with" 
     // Figure out if we're getting a template, or if we need to
     // load the template - and be sure to cache the result.
     var fn = !/\W/.test(str) ?
@@ -30,19 +31,18 @@ var hadashot = (function(jq,me){
       new Function("obj",
         "var p=[],print=function(){p.push.apply(p,arguments);};" +
        
-        // Introduce the data as local variables using with(){}
-        "with(obj){p.push('" +
+        "p.push('" +
        
         // Convert the template into pure JavaScript
         str
           .replace(/[\r\t\n]/g, " ")
-          .split("{{").join("\t")
-          .replace(/((^|}})[^\t]*)'/g, "$1\r")
-          .replace(/\t=(.*?)}}/g, "',$1,'")
+          .split("<%").join("\t")
+          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+          .replace(/\t=(.*?)%>/g, "',$1,'")
           .split("\t").join("');")
-          .split("}}").join("p.push('")
+          .split("%>").join("p.push('")
           .split("\r").join("\\'")
-      + "');}return p.join('');");
+      + "');return p.join('');");
    
     // Provide some basic currying to the user
     return data ? fn( data ) : fn;
