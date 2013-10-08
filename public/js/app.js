7fd5637cc4018e36de40852c2b34257479b8fbaa
Dotan Dimet <dotan@corky.net>
2013-10-08 21:20:32 +0300

    added moment.js for fancier time display; just in view/main for now; set blogroll as default route in view/ember

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
-  this.resource('blogroll');
+  this.resource('blogroll', { path: '/' });
 });
 App.BlogrollRoute = Ember.Route.extend({
   model: function() {
