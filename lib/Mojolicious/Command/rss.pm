f3493c66b6ed9c54f7b8223a12c9ea66ae866293
Dotan Dimet <dotan@corky.net>
2013-11-10 11:22:58 +0200

    fix rss command to work with FeedReader plugin

--- lib/Mojolicious/Command/rss.pm
+++ lib/Mojolicious/Command/rss.pm
@@ -5,45 +5,19 @@ use Hadashot;
 
 has app => sub { Mojo::Server->new->build_app('Hadashot') };
 
-has description => "fetch, parse and dump an rss feed";
+has description => "fetch, parse and dump an rss feed.\n";
 
 has usage => <<"EOT";
-usage: $0 rss URL OPTIONS
+usage: $0 rss URL 
 fetch rss feed from URL, parse and print Data::Dumper output
-Valid Options:
-	-last = print only dump of last parsed item
-  -reparse = parse the saved _raw output
 EOT
 
 sub run {
-	my ($self, @args) = @_;
-	my ($url, $last_only);
-  $last_only = 0;
-	for (@args) {
-		if (m/^\-last/) {
-			$last_only = 1;
-		}
-		else {
-			$url = $_;
-		}
-	}
-	my $tx = $self->app->ua->get($url);
-	if (my $res = $tx->success) {
-			$self->app->backend->parse_rss( $res->content->asset,
-			sub {
-				my ($item) = pop;
-				if ($last_only == 1) {
-					say $self->app->dumper($item);
-					$last_only = 2;
-				}
-				if ($last_only == 0) {
-				say $self->app->dumper($item);
-				}
-			} );
-		}
-		else {
-			say "Failed: ", $tx->error;
-		}
+	my ($self, $url) = @_;
+  my ($feed, $err, $code) = $self->app->parse_rss( Mojo::URL->new($url) );
+  say $self->app->dumper($feed) if ($feed);
+  say "Failed: $err " if ($err);
+  say "Got code $code" if ($code);
 }
 
 1;
